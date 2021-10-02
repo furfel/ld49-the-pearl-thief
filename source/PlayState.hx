95feb40747eb2c8f51c8d0bf3d3313b74593a739
pfoof <rotworm0@yahoo.pl>
2021-10-02 11:14:38 +0200

    created test pearls

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1,12 +1,18 @@
 package;
 
+import flixel.FlxG;
 import flixel.FlxState;
+import pearl.Pearl;
 
 class PlayState extends FlxState
 {
 	override public function create()
 	{
 		super.create();
+		FlxG.camera.antialiasing = true;
+		add(new Pearl(100, 100, 0xFFAAEE22));
+		add(new Pearl(200, 180, 0xFFFF2222));
+		add(new Pearl(180, 70, 0xFF99EEFF));
 	}
 
 	override public function update(elapsed:Float)
