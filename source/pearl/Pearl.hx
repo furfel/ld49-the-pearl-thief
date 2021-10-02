5057b1559b106cf6fe5cf9b8462276fcb944d06d
pfoof <rotworm0@yahoo.pl>
2021-10-03 01:56:15 +0200

    pearls position fix and alive handling

--- source/pearl/Pearl.hx
+++ source/pearl/Pearl.hx
@@ -3,6 +3,7 @@ package pearl;
 import flixel.FlxG;
 import flixel.FlxSprite;
 import flixel.group.FlxSpriteGroup.FlxTypedSpriteGroup;
+import flixel.math.FlxPoint;
 import flixel.tweens.FlxEase;
 import flixel.tweens.FlxTween;
 import flixel.util.FlxColor;
@@ -20,8 +21,8 @@ class Pearl extends FlxTypedSpriteGroup<FlxSprite>
 	public function new(X:Float, Y:Float)
 	{
 		super(X, Y);
-		add(pearlStand = new FlxSprite(X, Y, "assets/images/pearlstand.png"));
-		add(pearl = new FlxSprite(X, Y, "assets/images/pearl.png"));
+		add(pearlStand = new FlxSprite(0, 0, "assets/images/pearlstand.png"));
+		add(pearl = new FlxSprite(0, 0, "assets/images/pearl.png"));
 		hue = FlxG.random.float(0, 360);
 		pearl.color = FlxColor.fromHSL(hue, MAX_S, L);
 		pearl.x = pearlStand.x + pearlStand.width / 2 - pearl.width / 2;
@@ -51,4 +52,17 @@ class Pearl extends FlxTypedSpriteGroup<FlxSprite>
 	{
 		sat = MAX_S * percent;
 	}
+
+	override function kill()
+	{
+		pearl.alive = false;
+		pearl.exists = false;
+		pearlStand.alive = false;
+		pearlStand.exists = true;
+		this.alive = false;
+		this.exists = true;
+	}
+
+	public function getPearlMidpoint():FlxPoint
+		return pearl.getMidpoint();
 }
