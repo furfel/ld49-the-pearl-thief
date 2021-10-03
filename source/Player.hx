403a98d74c9697062f9d1e5bc9abd85cf402f977
pfoof <rotworm0@yahoo.pl>
2021-10-03 15:03:57 +0200

    player animation

--- source/Player.hx
+++ source/Player.hx
@@ -8,13 +8,66 @@ import flixel.util.FlxColor;
 
 class Player extends FlxSprite
 {
+	public static final ANGLES = [
+		FlxObject.RIGHT => 0,
+		FlxObject.RIGHT | FlxObject.DOWN => 45,
+		FlxObject.DOWN => 90,
+		FlxObject.DOWN | FlxObject.LEFT => 135,
+		FlxObject.LEFT => 180,
+		FlxObject.LEFT | FlxObject.UP => 225,
+		FlxObject.UP => 270,
+		FlxObject.UP | FlxObject.RIGHT => 315
+	];
+
 	public function new(X:Float, Y:Float)
 	{
 		super(X, Y);
-		makeGraphic(32, 32, FlxColor.PURPLE);
+		loadGraphic("assets/images/player.png", true, 32, 32);
+		var _directions = ["down", "rightdown", "right", "rightup", "up"];
+		for (_dir in 0..._directions.length)
+		{
+			var _d = _dir * 4;
+			var _n = _directions[_dir];
+			animation.add(_n, [0 + _d], 1);
+			animation.add(_n + "shoot", [1 + _d], 1, false);
+			animation.add(_n + "walk", [0 + _d, 2 + _d, 0 + _d, 3 + _d], 5);
+		}
+		for (_dir in ["down", "", "up"])
+			for (_n in ["", "shoot", "walk"])
+			{
+				var an = animation.getByName("right" + _dir + _n);
+				animation.add("left" + _dir + _n, an.frames.copy(), an.frameRate, an.looped, true);
+			}
+
+		setSize(20, 24);
+		offset.set(6, 8);
 	}
 
-	public static final PLAYER_SPEED = 300;
+	private var parent:PlayState;
+
+	public function setParent(parent:PlayState):Player
+	{
+		this.parent = parent;
+		return this;
+	}
+
+	private function setAnimationTo()
+	{
+		var _name = "";
+		if (facing & FlxObject.LEFT != 0)
+			_name += "left";
+		else if (facing & FlxObject.RIGHT != 0)
+			_name += "right";
+		if (facing & FlxObject.UP != 0)
+			_name += "up";
+		else if (facing & FlxObject.DOWN != 0)
+			_name += "down";
+		if (velocity.x != 0 || velocity.y != 0)
+			_name += "walk";
+		animation.play(_name);
+	}
+
+	public static final PLAYER_SPEED = 450;
 
 	private static function isLeft():Bool
 		return FlxG.keys.pressed.A || (FlxG.gamepads.firstActive != null && FlxG.gamepads.firstActive.pressed.DPAD_LEFT);
@@ -80,5 +133,8 @@ class Player extends FlxSprite
 	{
 		super.update(elapsed);
 		updateMovement();
+		setAnimationTo();
+		if (parent != null && FlxG.keys.justPressed.SPACE)
+			parent.shoot(getMidpoint().x, getMidpoint().y, ANGLES[facing]);
 	}
 }
