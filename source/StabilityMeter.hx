f615b5e0ebdec928ec2d75889829c49c85c291f6
pfoof <rotworm0@yahoo.pl>
2021-10-03 16:57:22 +0200

    added stability meter that looks just better

---
+++ source/StabilityMeter.hx
@@ -0,0 +1,45 @@
+package;
+
+import flixel.FlxG;
+import flixel.FlxSprite;
+import flixel.group.FlxSpriteGroup.FlxTypedSpriteGroup;
+import flixel.text.FlxText;
+import flixel.tweens.FlxEase;
+import flixel.tweens.FlxTween;
+
+class StabilityMeter extends FlxTypedSpriteGroup<FlxSprite>
+{
+	private var arrow:FlxSprite;
+	private var percentage:Float = 1.0;
+
+	public function new()
+	{
+		super(FlxG.width / 2, 64);
+		scrollFactor.set(0, 0);
+		add(new FlxSprite(0, 0, "assets/images/meter.png"));
+		add(arrow = new FlxSprite(64, 55, "assets/images/arrow.png"));
+		arrow.origin.set(0, 3.5);
+		arrow.scrollFactor.set(0, 0);
+		screenCenter(X);
+	}
+
+	private var tween:FlxTween;
+
+	override function update(elapsed:Float)
+	{
+		super.update(elapsed);
+		if (percentage < 0.35 && tween == null)
+		{
+			tween = FlxTween.angle(arrow, 0.0, -180.0, 1.0, {type: PINGPONG, ease: FlxEase.bounceInOut});
+		}
+		else if (tween == null)
+		{
+			arrow.angle = (1 - percentage) * -180.0;
+		}
+	}
+
+	public function setPercentage(p:Float)
+	{
+		percentage = p;
+	}
+}
