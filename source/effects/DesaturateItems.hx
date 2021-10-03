b65d5df59d15913c25ca7a023e048aa7df51583f
pfoof <rotworm0@yahoo.pl>
2021-10-03 16:58:44 +0200

    timebased stretch

--- source/effects/DesaturateItems.hx
+++ source/effects/DesaturateItems.hx
@@ -22,6 +22,7 @@ class DesaturateItems extends FlxBasic
 		sh = new FurfelMegaShader();
 		sh.data.saturation.value = [1.0];
 		sh.data.skew.value = [0.0];
+		sh.data.timeskew.value = [0.0];
 		this.parent.trees.shader = sh;
 		this.parent.treetops.shader = sh;
 	}
@@ -32,13 +33,14 @@ class DesaturateItems extends FlxBasic
 	{
 		super.update(elapsed);
 		phase += elapsed;
-		if (phase > 3.0)
-			phase -= 3.0;
+		if (phase > 6.5)
+			phase -= 6.5;
 		if (parent.getCurrentStability() < ACTIVATION_TRESHOLD)
 		{
 			var level = 1.0 - (ACTIVATION_TRESHOLD - parent.getCurrentStability()) / ACTIVATION_TRESHOLD;
 			sh.data.saturation.value = [level];
-			sh.data.skew.value = [1.0 - level + phase / 6.0];
+			sh.data.skew.value = [1.0 - level];
+			sh.data.timeskew.value = [phase];
 		}
 	}
 }
@@ -49,6 +51,7 @@ class FurfelMegaShader extends FlxGraphicsShader
     #pragma header
     uniform float saturation;
     uniform float skew;
+	uniform float timeskew;
     	attribute float alpha;
 		attribute vec4 colorMultiplier;
 		attribute vec4 colorOffset;
@@ -69,6 +72,7 @@ class FurfelMegaShader extends FlxGraphicsShader
 	@:glFragmentHeader("
         uniform float saturation;
         uniform float skew;
+		uniform float timeskew;
 
         // All components are in the range [0…1], including hue.
     vec3 rgb2hsv(vec3 c)
@@ -91,7 +95,7 @@ class FurfelMegaShader extends FlxGraphicsShader
     }
 		vec4 furfel_texture2D(sampler2D bitmap, vec2 coord)
 		{
-            float x_coord = clamp(coord.x - sin(coord.y * 18.0) * skew * 0.1, 0.0, 1.0);
+            float x_coord = clamp(coord.x - sin(coord.y * 18.0 + timeskew) * skew * 0.1, 0.0, 1.0);
             coord.x = x_coord;
 			vec4 color = flixel_texture2D(bitmap, coord);
             vec3 hsvcolor = rgb2hsv(color.rgb);
