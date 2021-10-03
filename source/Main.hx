2560a1217da24ba9d7343396533a89f384a0841c
pfoof <rotworm0@yahoo.pl>
2021-10-03 17:39:07 +0200

    start with different panel

--- source/Main.hx
+++ source/Main.hx
@@ -2,12 +2,13 @@ package;
 
 import flixel.FlxGame;
 import openfl.display.Sprite;
+import tutorial.Panel1;
 
 class Main extends Sprite
 {
 	public function new()
 	{
 		super();
-		addChild(new FlxGame(640, 480, PlayState, true));
+		addChild(new FlxGame(640, 480, Panel1, true));
 	}
 }
